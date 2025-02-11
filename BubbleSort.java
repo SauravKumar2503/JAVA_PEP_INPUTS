b62497732204a522e16ae25db150d48e6235efdf
Saurav Kumar <sourav2002kumar@gmail.com>
2025-02-11 09:26:12 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,33 @@
+//Modify Bubble sort so that if an array is already sorted before completing all passes,it stops early instead of running unnecessary iteration.Example: {1,2,3,4,5} should stop after 1st pass.
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        bubbleSort(arr);
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (swapped == false) {
+                break;
+            }
+        }
+        System.out.println("Sorted array: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
